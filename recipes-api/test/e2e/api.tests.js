ef2eb0d9e000dc63fbab2dce4c761ef089c4d28f
feliun <felipe.polo@gmail.com>
2017-06-12 22:25:06 +0200

    recipes-api mongo: refactor - testing all stores automatically

--- recipes-api/test/e2e/api.tests.js
+++ recipes-api/test/e2e/api.tests.js
@@ -1,72 +1,89 @@
 const R = require('ramda');
+const { join } = require('path');
 const expect = require('expect.js');
 const statusCodes = require('http-status-codes');
 const supertest = require('supertest-as-promised');
+const configSystem = require('../../components/config');
 const system = require('../../system');
-const inMemoryStore = require('../../components/lib/store/in-memory');
 const recipe = require('../../fixtures/recipe_sample.json');
-
-describe('In-memory Recipes API', () => {
-  let request;
-  let sys;
-  let myStore;
-
-  const normalise = R.omit('_id');
-
-  const mockFn = (system) =>
-    system()
-      .set('store', inMemoryStore());
-
-  before(done => {
-    sys = system(mockFn).start((err, { app, store }) => {
-      if (err) return done(err);
-      request = supertest(Promise)(app);
-      myStore = store;
-      done();
-    });
-  });
-
-  beforeEach(() =>
-    myStore.flush()
-  );
-
-  after(done => sys.stop(done));
-
-  const post = (recipe) =>
-    request
-      .post('/api/v1/recipes')
-      .send(recipe)
-      .expect(statusCodes.NO_CONTENT)
-
-  const get = (id, expectation) =>
-    request
-      .get(`/api/v1/recipes/${recipe.id}`)
-      .expect(expectation || statusCodes.OK)
-
-  const erase = (id) =>
-    request
-      .delete(`/api/v1/recipes/${recipe.id}`)
-      .expect(statusCodes.NO_CONTENT)
-
-  it('should POST a recipe', () => post(recipe));
-
-  it('should GET a recipe', () =>
-    post(recipe)
-      .then(() =>
-        get(recipe.id)
-          .then((response) => {
-            expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
-            expect(normalise(response.body)).to.eql(normalise(recipe));
-          })
-      )
-  );
-
-  it('should DELETE a recipe', () =>
-    post(recipe)
-      .then(() =>
-        erase(recipe.id)
-          .then(() => get(recipe.id, statusCodes.NOT_FOUND))
-      )
-  );
-
+const stores = require('require-all')({
+  dirname: join(__dirname, '..', '..', 'components', 'lib', 'store'),
+  filter: (fileName) => fileName === 'index.js' ? undefined : fileName.replace('.js', '')
 });
+
+const test = (store) => {
+
+  describe(`Recipes API based on ${store} store`, () => {
+    let request;
+    let sys;
+    let myStore;
+    let myConfig;
+
+    const normalise = R.omit('_id');
+
+    const mockFn = (system) =>
+      system()
+        .set('config', { start: (cb) => cb(null, myConfig) });
+
+    before(done => {
+      configSystem.start((err, { config }) => {
+        if (err) return done(err);
+        myConfig = R.merge(config, { store });
+        sys = system(mockFn).start((err, { app, store }) => {
+          if (err) return done(err);
+          request = supertest(Promise)(app);
+          myStore = store;
+          done();
+        });
+      });
+    });
+
+    beforeEach(() => myStore.flush());
+    after(done => sys.stop(done));
+
+    const post = (recipe) =>
+      request
+        .post('/api/v1/recipes')
+        .send(recipe)
+        .expect(statusCodes.NO_CONTENT)
+
+    const get = (id, expectation) =>
+      request
+        .get(`/api/v1/recipes/${recipe.id}`)
+        .expect(expectation || statusCodes.OK)
+
+    const erase = (id) =>
+      request
+        .delete(`/api/v1/recipes/${recipe.id}`)
+        .expect(statusCodes.NO_CONTENT)
+
+    it('should POST a recipe', () => post(recipe));
+
+    it('should GET a recipe', () =>
+      post(recipe)
+        .then(() =>
+          get(recipe.id)
+            .then((response) => {
+              expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
+              expect(normalise(response.body)).to.eql(normalise(recipe));
+            })
+        )
+    );
+
+    it('should DELETE a recipe', () =>
+      post(recipe)
+        .then(() =>
+          erase(recipe.id)
+            .then(() => get(recipe.id, statusCodes.NOT_FOUND))
+        )
+    );
+
+  });
+};
+
+const runAll = R.pipe(
+  R.keys,
+  R.map(test)
+);
+
+runAll(stores);
---
+++ recipes-api/test/unit/store/all.tests.js
@@ -0,0 +1,85 @@
+const R = require('ramda');
+const { join } = require('path');
+const expect = require('expect.js');
+const system = require('../../../system');
+const configSystem = require('../../../components/config');
+const recipe = require('../../../fixtures/recipe_sample.json');
+const stores = require('require-all')({
+  dirname: join(__dirname, '..', '..', '..', 'components', 'lib', 'store'),
+  filter: (fileName) => fileName === 'index.js' ? undefined : fileName.replace('.js', '')
+});
+
+const test = (store) => {
+  describe(`Testing store ${store}`, () => {
+    let myStore;
+    let sys;
+    let myConfig;
+
+    const mockFn = (system) =>
+      system()
+        .set('config', { start: (cb) => cb(null, myConfig) });
+
+    before(done => {
+      configSystem.start((err, { config }) => {
+        if (err) return done(err);
+        myConfig = R.merge(config, { store });
+        sys = system(mockFn).start((err, { store }) => {
+          if (err) return done(err);
+          myStore = store;
+          done();
+        });
+      });
+    });
+
+    beforeEach(() => myStore.flush());
+    after(done => sys.stop(done));
+
+    it('should throw an error when saving a recipe with no id', () =>
+      myStore.saveRecipe(R.omit('id',recipe))
+        .catch((err) => expect(err.message).to.equal('Could not save recipe with no id'))
+    );
+
+    it('should save a recipe when the recipe does not exist', () =>
+      myStore.saveRecipe(recipe)
+        .then(() => myStore.getRecipe(recipe.id))
+        .then((saved) => expect(saved).to.eql(recipe))
+    );
+
+    it('should update a recipe when the recipe exists and the new version is greater than the saved one', () => {
+      const greaterVersion = new Date().getTime();
+      const update = R.merge(recipe, { version: greaterVersion });
+      return myStore.saveRecipe(recipe)
+        .then((saved) => myStore.saveRecipe(update))
+        .then((update) => myStore.getRecipe(recipe.id))
+        .then((saved) => expect(saved.version).to.eql(greaterVersion))
+    });
+
+    it('should not update a recipe when the recipe exists and the new version is lower than the saved one', () => {
+      const lowerVersion = 1;
+      const update = R.merge(recipe, { version: lowerVersion });
+      return myStore.saveRecipe(recipe)
+        .then(() => myStore.saveRecipe(update))
+        .then(() => myStore.getRecipe(recipe.id))
+        .then((saved) => expect(saved.version).to.eql(recipe.version))
+    });
+
+    it('should throw an error when deleting a recipe with no id', () =>
+      myStore.deleteRecipe(null)
+        .catch((err) => expect(err.message).to.equal('Could not delete recipe with no id'))
+    );
+
+    it('should delete a recipe', () =>
+      myStore.saveRecipe(recipe)
+        .then(() => myStore.deleteRecipe(recipe.id))
+        .then(() => myStore.getRecipe(recipe.id))
+        .then((saved) => expect(saved).to.eql(null))
+    );
+  });
+};
+
+const runAll = R.pipe(
+  R.keys,
+  R.map(test)
+);
+
+runAll(stores);
