2bbd4501e13b92a0f9ceafbc3cf1a4bbb8d0da76
feliun <felipe.polo@gmail.com>
2017-06-26 20:35:30 +0200

    recipes-crawler: moving rabbit test config into test.json

--- recipes-crawler/config/default.js
+++ recipes-crawler/config/default.js
@@ -62,37 +62,9 @@ module.exports = {
           'retry',
           'dead_letters'
         ],
-        queues: {
-          'dead_letters:snoop': {},
-          'retry:snoop': {},
-          'delay:1ms': {
-            options: {
-              arguments: {
-                'x-message-ttl': 1,
-                'x-dead-letter-exchange': 'retry'
-              }
-            }
-          },
-          'recipes_crawler:snoop': {}
-        },
-        bindings: {
-          'delay[delay.#] -> delay:1ms': {},
-          'retry -> retry:snoop': {},
-          'dead_letters -> dead_letters:snoop': {},
-          'internal[recipes_crawler.v1.notifications.#.#] -> recipes_crawler:snoop': {}
-        },
-        subscriptions: {
-          dead_letters: {
-            queue: 'dead_letters:snoop'
-          },
-          retries: {
-            queue: 'retry:snoop'
-          },
-          recipes_crawler: {
-            queue: 'recipes_crawler:snoop',
-            contentType: 'application/json'
-          }
-        },
+        queues: {},
+        bindings: {},
+        subscriptions: {},
         publications: {
           conclusions: {
             exchange: 'internal'
--- recipes-crawler/config/test.js
+++ recipes-crawler/config/test.js
@@ -9,5 +9,57 @@ module.exports = {
     autostart: true,
     key: 'some_key',
     page: 10
+  },
+  rabbitmq: {
+    defaults: {
+      vhosts: {
+        namespace: true,
+        queues: {
+          options: {
+            durable: false
+          }
+        },
+        exchanges: {
+          options: {
+            durable: false
+          }
+        }
+      }
+    },
+    vhosts: {
+      '/': {
+        queues: {
+          'dead_letters:snoop': {},
+          'retry:snoop': {},
+          'delay:1ms': {
+            options: {
+              arguments: {
+                'x-message-ttl': 1,
+                'x-dead-letter-exchange': 'retry'
+              }
+            }
+          },
+          'recipes_crawler:snoop': {}
+        },
+        bindings: {
+          'delay[delay.#] -> delay:1ms': {},
+          'retry -> retry:snoop': {},
+          'dead_letters -> dead_letters:snoop': {},
+          'internal[recipes_crawler.v1.notifications.#.#] -> recipes_crawler:snoop': {}
+        },
+        subscriptions: {
+          dead_letters: {
+            queue: 'dead_letters:snoop'
+          },
+          retries: {
+            queue: 'retry:snoop'
+          },
+          recipes_crawler: {
+            queue: 'recipes_crawler:snoop',
+            contentType: 'application/json'
+          }
+        },
+      }
+    }
   }
 };
