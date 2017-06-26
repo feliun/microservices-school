0322ffbdb96f3c167d8620e7f6b68df2f84152a6
feliun <felipe.polo@gmail.com>
2017-06-26 20:32:27 +0200

    recipes-api: moving rabbit test config into test.json

--- recipes-api/config/default.js
+++ recipes-api/config/default.js
@@ -60,37 +60,9 @@ module.exports = {
           'retry',
           'dead_letters'
         ],
-        queues: {
-          'dead_letters:snoop': {},
-          'retry:snoop': {},
-          'delay:1ms': {
-            options: {
-              arguments: {
-                'x-message-ttl': 1,
-                'x-dead-letter-exchange': 'retry'
-              }
-            }
-          },
-          'recipes_api:snoop': {}
-        },
-        bindings: {
-          'delay[delay.#] -> delay:1ms': {},
-          'retry -> retry:snoop': {},
-          'dead_letters -> dead_letters:snoop': {},
-          'internal[recipes_api.v1.notifications.#.#] -> recipes_api:snoop': {}
-        },
-        subscriptions: {
-          dead_letters: {
-            queue: 'dead_letters:snoop'
-          },
-          retries: {
-            queue: 'retry:snoop'
-          },
-          recipes_api: {
-            queue: 'recipes_api:snoop',
-            contentType: 'application/json'
-          }
-        },
+        queues: {},
+        bindings: {},
+        subscriptions: {},
         publications: {
           conclusions: {
             exchange: 'internal'
--- recipes-api/config/test.js
+++ recipes-api/config/test.js
@@ -17,6 +17,41 @@ module.exports = {
           }
         }
       }
+    },
+    vhosts: {
+      '/': {
+        queues: {
+          'dead_letters:snoop': {},
+          'retry:snoop': {},
+          'delay:1ms': {
+            options: {
+              arguments: {
+                'x-message-ttl': 1,
+                'x-dead-letter-exchange': 'retry'
+              }
+            }
+          },
+          'recipes_api:snoop': {}
+        },
+        bindings: {
+          'delay[delay.#] -> delay:1ms': {},
+          'retry -> retry:snoop': {},
+          'dead_letters -> dead_letters:snoop': {},
+          'internal[recipes_api.v1.notifications.#.#] -> recipes_api:snoop': {}
+        },
+        subscriptions: {
+          dead_letters: {
+            queue: 'dead_letters:snoop'
+          },
+          retries: {
+            queue: 'retry:snoop'
+          },
+          recipes_api: {
+            queue: 'recipes_api:snoop',
+            contentType: 'application/json'
+          }
+        }
+      }
     }
   }
 };
