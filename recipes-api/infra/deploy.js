87392c575a1305e21e0c78ef0a9a6b1e8e58522e
feliun <felipe.polo@gmail.com>
2017-08-06 22:05:36 +0200

    CD: files cleanup

--- recipes-api/infra/deploy.js
+++ recipes-api/infra/deploy.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { join } = require('path');
 const { downloadPemFile, downloadInstanceRegistry } = require('./s3');
 const { replaceServiceContainer } = require('./ssh');
@@ -7,6 +8,12 @@ if (!process.env.SERVICE) throw new Error("No SERVICE environment variable has b
 const PEM_KEY_PATH = join(__dirname, 'micro-school-ec2.pem');
 const INSTANCE_REGISTRY_PATH = join(__dirname, 'instances-registry.json');
 
+const removeFile = (filePath) => new Promise((resolve, reject) => {
+  fs.unlink(filePath, (err) => (err ? reject(err) : resolve()));
+});
+
+const cleanUp = () => Promise.all([ removeFile(PEM_KEY_PATH), removeFile(INSTANCE_REGISTRY_PATH) ]);
+
 Promise.all([
   downloadPemFile(PEM_KEY_PATH),
   downloadInstanceRegistry(INSTANCE_REGISTRY_PATH)
@@ -14,10 +21,11 @@ Promise.all([
   .then((result) => {
     const { publicDns } = require(INSTANCE_REGISTRY_PATH);
     return replaceServiceContainer(publicDns, PEM_KEY_PATH, process.env)
+      .then(() => cleanUp())
       .then(() => {
         console.log(`The service ${process.env.SERVICE} has been deployed`);
         process.exit(0)
-      })// TODO delete temp files
+      })
   })
   .catch((err) => {
     console.error(err);
