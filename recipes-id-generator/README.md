b8772eca0db39a90a1d4489e8b620806818e8fea
feliun <felipe.polo@gmail.com>
2017-08-17 21:33:12 +0200

    recipes-id-generator: Fixing service name

--- recipes-id-generator/README.md
+++ recipes-id-generator/README.md
@@ -1,3 +1,3 @@
-# recipes-ip-generator
+# recipes-id-generator
 
 A service to generate ids sequentially in a microservice ecosystem
--- recipes-id-generator/system.js
+++ recipes-id-generator/system.js
@@ -1,4 +1,4 @@
 const System = require('systemic');
 const { join } = require('path');
 
-module.exports = () => new System({ name: 'recipes-ip-generator' }).bootstrap(join(__dirname, 'components'));
+module.exports = () => new System({ name: 'recipes-id-generator' }).bootstrap(join(__dirname, 'components'));
