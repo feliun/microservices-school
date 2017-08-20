99bd7aa4dd0bf3a391da93cea47c36e8ee311289
feliun <felipe.polo@gmail.com>
2017-08-20 17:38:50 +0200

    recipes-api: fixing test

--- recipes-api/components/logging/sumo.js
+++ recipes-api/components/logging/sumo.js
@@ -1,3 +1,4 @@
+const R = require('ramda');
 const Sumologic = require('logs-to-sumologic');
 
 let logger;
@@ -12,7 +13,7 @@ module.exports = () => {
 
   const start = (cb) => {
     const url = process.env.SUMO_URL;
-    if (!url) return cb(new Error('A SUMO_URL env variable needs to be injected!'));
+    if (!url) return cb(null, R.identity);
     logger = Sumologic.createClient({ url, name: "RecipesCollector" });
     return cb(null, onMessage);
   };
