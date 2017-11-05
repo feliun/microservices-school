1eceb79d6f63f634c89907ff07059bae98ef3d88
feliun <felipe.polo@gmail.com>
2017-11-05 17:34:21 +0100

    Deleting all ops-related code

--- recipes-crawler/infra/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 3001:3001 -e SERVICE_ENV=live -e F2F_KEY=$F2F_KEY -e RABBIT_PWD=$RABBIT_PWD -e SUMO_URL=$SUMO_URL --name recipes-crawler --network=local quay.io/feliun/recipes-crawler:latest
--- recipes-id-generator/infra/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 3002:3002 -e SERVICE_ENV=live -e MONGO_URL=$MONGO_URL -e SUMO_URL=$SUMO_URL --name recipes-id-generator --network=local quay.io/feliun/recipes-id-generator:latest
