18aeb5d480e76297386fe67dfebb481ec86144c3
feliun <felipe.polo@gmail.com>
2018-11-25 18:13:19 +0100

    Improving ubuntu setup

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -6,19 +6,23 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 sudo apt-add-repository "deb https://download.sublimetext.com/ apt/stable/"
 sudo apt install sublime-text
 
-#installing nodejs
+#installing nvm
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 source ~/.profile
-nvm install 10.5.0
 node -v
 
-#installing docker-compose
+#installing docker & docker-compose
+apt install docker.io
 sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -v
+
+#installing nodejs environment
 nvm install 7
 nvm use 7
+
+#building project
 npm install -g yarn
 pushd recipes-api
 yarn install
@@ -32,3 +36,8 @@ popd
 pushd recipes-infra
 yarn install
 popd
+
+#downloading all branches
+git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+git fetch --all
+git pull --all
